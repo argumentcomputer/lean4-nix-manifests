947e55ce2c6ae157c92ca3bc690fc684a18e26b7
Samuel Burnham <45365069+samuelburnham@users.noreply.github.com>
2025-06-27 15:39:39 -0400

    v4.16.0

--- bootstrap.nix
+++ bootstrap.nix
@@ -1,6 +1,6 @@
 {
-  tag = "v4.15.0";
-  rev = "11651562caae0a0b3973811508db2ab8903d3854";
+  tag = "v4.16.0";
+  rev = "128a1e6b0a82718382f9c39c79be44ff3284547c";
   bootstrap = {
     src,
     debug ? false,
